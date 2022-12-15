577db0ec27d85cb7d78f59bfb765942ee7efc9da
Veeramani4156 <veeramani4156@gmail.com>
2022-12-15 18:46:48 +0530

    n variable changed to x

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,11 +5,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.println("Enter a number:  ");
-        int n = s.nextInt();
+        int x = s.nextInt();
         int a=1, b=1, c;
         System.out.println(a);
         System.out.println(b);
-        for(int i=2; i<=n; i++){
+        for(int i=2; i<=x; i++){
             c= a+b;
             System.out.println(c);
             a=b;
