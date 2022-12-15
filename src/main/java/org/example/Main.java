93337597ac49fa0c870db9a69b94d287d15db9f2
Veeramani4156 <veeramani4156@gmail.com>
2022-12-15 15:39:34 +0530

    fibonacci series with int n = 9

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,17 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int n = 9;
+        int a=1, b=1, c;
+        System.out.println(a);
+        System.out.println(b);
+        for(int i=2; i<=9; i++){
+            c= a+b;
+            System.out.println(c);
+            a=b;
+            b=c;
+
+        }
     }
 }
